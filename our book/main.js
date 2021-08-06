8ba7c7ea38e8a8718ab7871eefbd1e9c5bd3ebb7
Abdullah-git-hub <arafat200423@gmail.com>
2021-08-06 15:22:49 +0600

    merging to new site

--- our book/main.js
+++ our book/main.js
@@ -3,4 +3,31 @@ let form = document.querySelector('.form');
 
 btn.addEventListener("click",()=>{
     form.classList.add('block');
-})
+});
+
+
+
+const img = $("#bookImage");
+const disImg = $("#upload-img");
+
+img.on("change", function () {
+    imgFile = this.files[0];
+    // previewImgCon.style.display = "block";
+    // previewBack.style.display = "block";
+
+    if (imgFile != undefined) {
+        let reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+            // previewImg.setAttribute("src", this.result);
+            disImg.attr("src", this.result);
+            console.log(this.result)
+        });
+
+        reader.readAsDataURL(imgFile);
+
+    } else if (imgFile == undefined) {
+        photoInpt.files[0] = null;
+        disImg.attr("src", "");
+    }
+});
